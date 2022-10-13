00824e8d46b849b1ef3709421fd7436ad227f4a6
Rahwa26Mulugeta <mulugeta_rahwa@yahoo.com>
2022-10-13 12:20:33 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - It is responsible for startinf and terminating a program
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle/n");
+	return (0);
+}
